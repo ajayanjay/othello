bd66d264a3101885042cd1c10ded1633274d1fb1
ajayanjay <muhammad.azzar.tif424@polban.ac.id>
2025-05-25 12:43:46 +0700

    adjutment to isGameOver method

--- game.c
+++ game.c
@@ -1,8 +1,9 @@
 #include "game.h"
+#include "piece.h"
 #include <stdlib.h>
 
 int isGameOver(NodeOctuple * board) {
-    return (isBoardFull(board) || !hasValidMove(board, 'O') && !hasValidMove(board, 'X'));
+    return (isBoardFull(board) || (!hasValidMove(board, BLACK) && !hasValidMove(board, WHITE)));
 }
 
 int hasValidMove(NodeOctuple * board, char player) {
